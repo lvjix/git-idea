db2b3b26dcbb8d2128efd64e9c4ae3dd6a7547a4
lvjixiang <jy201019@163.com>
2022-04-01 16:00:18 +0800

    dev第一次提交

--- src/main/java/cn/jx/git/Test.java
+++ src/main/java/cn/jx/git/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static  void main(String[] args){
         System.out.println("hello git");
         System.out.println("HELLO DEV");
+        System.out.println("第四次提交");
     }
 }
